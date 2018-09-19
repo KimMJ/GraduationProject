a0ab83bccc8d0b2630a9c9d465f3973995d3a7e6
KimMJ <ss59s@nate.com>
2018-09-20 01:07:12 +0900

    - modify client.cpp to be single thread

--- epoll/client.cpp
+++ epoll/client.cpp
@@ -19,13 +19,13 @@
 
 void *send_data(void *arg);
 void *recv_data(void *arg);
-//void *alarm_timer(void *arg);
+void *client_process(void *arg);
+
 using namespace std;
 using namespace cv;
 
 #define INPUT "../cctv.avi"
-#define OUTPUT_PREFIX "output"
-#define OUTPUT_POSTFIX ".jpg"
+#define OUTPUT_FILENAME "output.jpg"
 
 bool socket_connected = false;
 int expire = DEFAULT_EXPIRE;
@@ -35,7 +35,7 @@ enum Light cur_light = RED;
 int main(int argc, char **argv){
   int sock;
   struct sockaddr_in server_address;
-  pthread_t snd_thread, rcv_thread, alarm_thread;
+  pthread_t snd_thread, rcv_thread, process_thread;
   void *thread_result;
 
   if (!(argc == 3)){
@@ -60,13 +60,13 @@ int main(int argc, char **argv){
   }
   socket_connected = true;
 
-  pthread_create(&snd_thread, NULL, send_data, (void *) &sock);
-  pthread_create(&rcv_thread, NULL, recv_data, (void *) &sock);
-  //pthread_create(&alarm_thread, NULL, alarm_timer, (void *) &sock);
+  //pthread_create(&snd_thread, NULL, send_data, (void *) &sock);
+  //pthread_create(&rcv_thread, NULL, recv_data, (void *) &sock);
+  pthread_create(&process_thread, NULL, client_process, (void *) &sock);
 
-  pthread_join(snd_thread, &thread_result);
-  pthread_join(rcv_thread, &thread_result);
-  //pthread_join(alarm_thread, &thread_result);
+  //pthread_join(snd_thread, &thread_result);
+  //pthread_join(rcv_thread, &thread_result);
+  pthread_join(process_thread, &thread_result);
   
   close(sock);
   return 0;
@@ -133,13 +133,6 @@ void *send_data(void *arg){
         exit(1);
       }
 
-      //scp
-      /*
-      int ret = system("scp output.jpg kimmj@10.78.152.63:~/raspberryPi/epoll/output3.jpg");
-      if (ret != 0) {
-        printf("some error occured!\n");
-      }
-      */
       //notice transfer
       memset(data, 0, BUFSIZE);
       sprintf(data, "%010lu", file_len);
@@ -154,7 +147,7 @@ void *send_data(void *arg){
       }
       printf("done!\n");
     } 
-    /*
+    /* //chat
     else {
       sprintf(data, "%s", data);
       write(sock, data, strlen(data));
@@ -167,25 +160,7 @@ void *recv_data(void *arg){
   int sock = *(int *) arg;
   char data[BUFSIZE];
   int len = 0;
-  /*
-  while (true){
-    memset(data, 0, BUFSIZE);
 
-    len = recv(sock, data, BUFSIZE, 0);
-    
-    if (len == -1){
-      return (void *) 1;
-    }
-    
-    if (len == 0){
-      printf("socket closed\n");
-      socket_connected = false;
-      return (void *) 1;
-    }
-    printf("new expire : %d\n", atoi(data));
-    expire = atoi(data);
-  }
-  */
   while (true) {
     memset(data, 0, BUFSIZE);
 
@@ -221,24 +196,94 @@ void *recv_data(void *arg){
       }
     }
   }
+  return (void*) 1;
 }
 
-/*
-void *alarm_timer(void *arg) {
-  while (true) {
-    while (expire > cur_timer){
-      sleep(1);
-      cur_timer ++;
+void *client_process(void *arg) {
+  int sock = *(int*) arg;
+  char buf[BUFSIZE];
+  int len = -1;
+  int fd = -1;
+
+  //OPENCV
+  int currentFrame = 0;
+  VideoCapture vc = VideoCapture(INPUT);
+
+  if (!vc.isOpened()) {
+    cerr << "fail to open the video" << endl;
+    return (void*) EXIT_FAILURE;
+  }
+
+  while (socket_connected == true) {
+    double fps = vc.get(CV_CAP_PROP_FPS);
+
+    cout << "total video frame : " << vc.get(CV_CAP_PROP_FRAME_COUNT) << endl;
+
+    Mat frame;
+    vc >> frame;
+
+    if (frame.empty()) {
+      printf("video is end\n");
+      exit(0);
     }
-    if (cur_light == RED) {
-      cur_light = GREEN;
-      printf("RED to GREEN\n");
-    } else {
-      cur_light = RED;
-      printf("GREEN to RED\n");
+    
+    imshow("image", frame);
+    imwrite(OUTPUT_FILENAME, frame);
+
+    FILE * f = fopen(OUTPUT_FILENAME, "r");
+    fseek(f, 0, SEEK_END);
+    unsigned long file_len = (unsigned long) ftell(f);
+
+    fd = open(OUTPUT_FILENAME, O_RDONLY);
+
+    if (fd == -1) {
+      printf("fatal error : no file error\n");
+      exit(1);
     }
-    cur_timer = 0;
-    expire = DEFAULT_EXPIRE;
+
+    memset(data, 0, BUFSIZE);
+    sprintf(data, "%010lu", file_len);
+    printf("%s file size : %s\n", OUTPUT_FILENAME, data);
+    send(sock, data, strlen(data), 0);
+
+    while ((len = read(fd, data, BUFSIZE)) != 0) {
+      send(sock, data, len, 0);
+    }
+
+    printf("file transfer is done\n");
+
+    memset(data, 0, BUFSIZE);
+
+    len = recv(sock, data, BUFSIZE, MSG_DONTWAIT);//non_blocking
+
+    if (len == 0) {
+      //socket is closed.
+      printf("socket is closed\n");
+      socket_connected = false;
+      break;
+    }
+    // if some data received
+    else if (len > 0) {
+      printf("new expire : %d\n", atoi(data));
+      expire = atoi(data);
+    }
+    //error
+    else {
+      // case 1 : no data
+      if (expire > cur_timer) {
+        sleep(1);
+        cur_timer ++;
+      }
+      else {
+        if (cur_light == RED) {
+          printf("RED to GREEN\n");
+        } else {
+          cur_light = RED;
+          printf("GREEN to RED\n");
+        }
+        cur_timer = 0;
+        expire = DEFAULT_EXPIRE;
+      }
+    }
   }
 }
-*/
